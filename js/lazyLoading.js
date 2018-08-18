14fadf8ebb33d0a8417ad6be8d4129b925189c40
Islam <islam.sayed8@gmail.com>
2018-08-18 15:44:09 +0200

    add lazy loading

---
+++ js/lazyLoading.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.lazy').Lazy({
+        effect: 'fadeIn',
+        effectTime: 500,
+        threshold: 0
+    });
+    $('.lazy2').Lazy({
+        effect: 'fadeIn',
+        effectTime: 1000,
+        threshold: 0
+    });
+});
---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    //listen to scroll and show the button when scrolling more than 20
+    window.onscroll = () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById('scollTotopBtn').style.display = "block";
+        } else {
+            document.getElementById('scollTotopBtn').style.display = "none";
+        }
+       
+    }
+    var scrollToTopTimeInterval;
+    document.getElementById('scollTotopBtn').addEventListener('click', () => {
+        //To make scrolling to top not immediate.
+        scrollToTopTimeInterval = setInterval(scrollToTop, 10)
+        function scrollToTop() {
+            document.body.scrollTop -= 50; // For Safari
+            document.documentElement.scrollTop -= 50; // For Chrome, Firefox, IE and Opera
+            console.log(document.body.scrollTop,document.documentElement.scrollTop)
+            if (document.body.scrollTop === 0 && document.documentElement.scrollTop === 0) {
+                clearInterval(scrollToTopTimeInterval)
+            }
+        }
+    })
+}
+
+
+    
