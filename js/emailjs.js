85f44997dfc6ff0608cb2468e9b607bc0661ba21
Islam <islam.sayed8@gmail.com>
2018-08-20 10:55:35 +0200

    Add emailjs service

---
+++ js/emailjs.js
@@ -0,0 +1,14 @@
+(function(){
+    emailjs.init("user_lqKZ5vejfBA034vAQhC0C");
+ })();
+
+ window.onload = function() {
+    document.getElementById('hire-application').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('contact_service', 'hireus_template', this);
+    });
+    document.getElementById('job-application').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('contact_service', 'jobapply_template', this);
+    });
+}
